9cf0e6e6f8c0788db463cbe95851a91fffb6ba14
xolesteroll <xol2dem@gmail.com>
2021-08-03 10:43:15 +0300

    first commit

---
+++ my-register-post-types.php
@@ -0,0 +1,21 @@
+<?php
+ 
+/*
+ 
+Plugin Name: My Register Post Types Plugin
+ 
+Plugin URI: https://github.com/xolesteroll/my-wp-plugin
+ 
+Description: This is my first WordPress plugin
+ 
+Version: 1.0
+ 
+Author: Anton
+ 
+Author URI: https://github.com/xolesteroll
+ 
+License: GPLv2 or later
+ 
+Text Domain: xolesteroll
+ 
+*/
